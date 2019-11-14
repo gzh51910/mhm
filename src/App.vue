2bce0c0d9db6c7b7a031ff4bec361f791ce76e2c
Errors_bug <1150691586@qq.com>
2019-11-14 09:34:56 +0800

    编写接口

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
-
+    <header class="header">
+      <img src="./img/mhmlogo.png" alt />
+      <h1>MHM游戏交易</h1>
+      <a href class="iconfont icon-liebiao"></a>
+      <a href class="iconfont icon-sousuo"></a>
+    </header>
     <main>
       <router-view />
     </main>
@@ -11,26 +16,53 @@
 // import {mapMutations} from 'vuex';
 
 export default {
-  name: 'app',
-  data(){
-    return {
-
-    }
+  name: "app",
+  data() {
+    return {};
   },
-  components: {
-    
-  },
-  computed:{
-
-  },
-  methods:{
-    
-  }
-}
+  components: {},
+  computed: {},
+  methods: {}
+};
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  
+  .header {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 44px;
+    background: #14b9c8;
+    //   text-align: center;
+    img {
+      margin-top: 8px;
+      width: 75px;
+      height: 29px;
+      float: left;
+      margin-left: 10px;
+    }
+    h1 {
+      margin: 0;
+      padding: 0;
+      height: 44px;
+      box-sizing: border-box;
+      float: left;
+      font-size: 18px;
+      color: white;
+      line-height: 44px;
+      margin-left: 44px;
+    }
+    a {
+      text-decoration: none;
+      color: white;
+      float: right;
+      font-size: 32px;
+      line-height: 44px;
+      &:nth-of-type(1) {
+        margin-right: 10px;
+      }
+    }
+  }
 }
 </style>
--- src/pages/Client.vue
+++ src/pages/Client.vue
@@ -1,11 +1,32 @@
 <template>
-    <div>端游专区</div>
+  <div>
+    <!-- <div v-for="item in datalist" :key="item.title">
+      <img :src="item.src" alt />
+    </div> -->
+  </div>
 </template>
 <script>
 export default {
-    
-}
+  data() {
+    return {
+      datalist: []
+    };
+  },
+  methods: {},
+  async created() {
+    let {
+      data: { data }
+    } = await this.$axios.get("http://localhost:1910/goods", {
+      params: {
+        gather: "Client"
+      }
+    });
+    this.datalist = data.map(item => {
+      return item;
+    });
+    console.log(this.datalist);
+  }
+};
 </script>
 <style lang="scss">
-
 </style>
